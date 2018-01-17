006a26f93c1b36b65ae7f2442438b46c38a9025b
mischastik <michael.balda@gmail.com>
2018-01-17 21:40:28 +0100

    [bugfix]Wrong parameter was given to ClculateJacobian

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -145,7 +145,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
-    Hj_ = tools.CalculateJacobian(measurement_pack.raw_measurements_);
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
     // Set up measurement noise matrix R
     ekf_.H_ = Hj_;
     ekf_.R_ = R_radar_;
